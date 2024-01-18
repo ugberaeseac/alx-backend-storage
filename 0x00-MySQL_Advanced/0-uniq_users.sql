93b8352d5f16903ed65027a9afca87cc8891581d
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-18 08:51:26 -0500

    An SQL script that creates a table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
-- SQL script that creates a table
-- if table already exists, script should not fail
-- script can be executed on any database
+-- SQL script that creates a table
+-- if table already exists, script should not fail
+-- script can be executed on any database
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY AUTO INCREMENT,
-	email varchar(256) NOT NULL UNIQUE,
-	name varchar (256)
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar (255)
 	);
