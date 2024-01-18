52e175222ae67f6092e659cb8d9263ca85e7128a
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-18 09:10:33 -0500

    An SQL script that creates a table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
-	name varchar (255)
+	name varchar (255),
 	country ENUM('US', 'CO', 'TN') NOT NULL
 	);
