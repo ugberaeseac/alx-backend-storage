a564f604c76bf2cd7b612d38f05971b979845565
UGBERAESE Charles A <ugberaeseac@gmail.com>
2024-01-23 20:18:47 -0800

    Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that lists all documents in a collection
+returns an empty list if no document in the collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    list all documents in collection
+    """
+    if mongo_collection is not None:
+        return mongo_collection.find()
+    else:
+        return []
