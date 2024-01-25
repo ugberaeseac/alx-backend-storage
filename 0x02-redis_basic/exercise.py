d0070f78aae9d7b3df8de428c36041c155029044
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-24 23:58:14 -0500

    incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,22 @@ returns the random key
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
 
+
+def count_calls(self, method: Callable) -> Callable:
+    """
+    count how many times methods of the Cache class are called.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
 
 class Cache():
     """
@@ -22,7 +37,8 @@ class Cache():
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         returns redis string
@@ -50,14 +66,13 @@ class Cache():
         val = self._redis.get(key).decode('utf-8')
         return val
 
-  """  def get_int(self, key: str) -> int:
-        
+    def get_int(self, key: str) -> int:
+        """
         parametrize to an integer
-        
+        """
         val = self._redis.get(key)
         try:
             int_val = int(val.decode('utf-8'))
         except Exception:
             int_val = 0
         return int_val
-        """
