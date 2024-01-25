e4959c7f985aabe4783e5e07e3f1ddab213388e7
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-01-24 23:37:16 -0500

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ returns the random key
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache():
@@ -25,9 +25,34 @@ class Cache():
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-
+        returns redis string
         """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union(str, bytes, int, float]:
+        """
+        convert the data back to the desired format
+        """
+        val = self._redis.get(key)
+        if fn is not None:
+            val = fn(value)
+
+        return val
+
+    def get_str(self, key: str) -> str:
+        """
+        parametrize to a string
+        """
+        val = self._redis.get(key).decode('utf-8')
+        return val
+
+    def get_int(self, key: str) -> int:
+        """
+        parametrize to an integer
+        """
+        val = self.redis.get(key).decode('utf-8')
+        return int(val)
